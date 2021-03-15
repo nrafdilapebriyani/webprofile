d99ff0436a2772239ab77b6d6fcb994fc4adaf21
nrafdilapebriyani <nrafdilafebri29@gmailcom>
2021-03-15 23:48:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratikum 2</title>
+</head>
+
+<body>
+    <section style="text-align: center;">
+        <img style="border-radius: 10%; width: 50%;" src="gambar/profil.jpg" alt="">
+        <h1 style="font-size: 36px;font-weight: bold;color: purple;font-style: italic;"> nur afdila</h1>
+        <h1 style="font-size: 16px;font-weight: bold;color: purple;font-style: italic;">calon programmer</h1>
+        <p style="font-size: 8xp; color:darkgreen;">universitas muhammadiyah yogyakarta</p>
+        <a href="https://www.facebook.com/afdilla.afdilla.3139">
+            <img style="border-radius: 100%; width: 10%; " src="gambar/fb.png" alt="">
+        </a>
+        <a href="https://www.instagram.com/nrafdilla12/">
+            <img style="border-radius: 100%; width: 9%; " src="gambar/ig.png" alt="">
+        </a>
+
+    </section>
+    
+    <section>
+        <center>
+        <table style="border: 10px solid rgb(247, 7, 155);">
+            <tr>
+                <th>tempat lahir</th>
+                <th>tanggal lahir</th>
+                
+            </tr>
+            <tr>
+                <td>medan</td>
+                <td>12 februari 2001</td>
+                
+            </tr>
+        </table>
+    </center>
+    </section>
+    <section>
+        <h3>riwayat sekolah</h3>
+
+        <ul>
+            <li>SD Al falah</li>
+            <li>SMP N 1 AEK LEDONG</li>
+            <li>SMA MUHAMMADIYAH 09 AEK KANOPAN</li>
+        </ul>
+        <h3>pengalaman organisasi</h3>
+
+        <ul>
+            <li>PRAMUKA</li>
+            <li>NASYID</li>
+        </ul>
+        <h3>sertifikat</h3>
+
+        <ul>
+            <li>OLIMPIADE BAHASA INGGRIS</li>
+            <li>OLIMPIADE MATE-MATIKA</li>
+        </ul>
+
+        <h3>motifasi belajar</h3>
+        <p>biar jadi jadi orang sukses</p>
+
+
+    </section>
+    
+</body>
+
+</html>
